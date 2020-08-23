e26bd8e21664df6ce782b69aafe5e099105ebf52
Cobrettie <cobrettiegarner3@gmail.com>
2020-08-23 17:16:24 -0400

    configure 'move_to_front' 'move_to_endl methods

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -99,14 +99,18 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
+        val = node.value
+        self.delete(node)
+        self.add_to_head(val)
         
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        val = node.value
+        self.delete(node)
+        self.add_to_tail(val)
 
     """
     Deletes the input node from the List, preserving the 
